7462aec6202beecfb9ebc9a2d68ec3f8cf1ce39b
Sebastian Czech <czechsebastian@icloud.com>
2019-05-16 15:47:32 -0400

    push at 4pm

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ Ingredient.create(name: "Light rum")
+ Ingredient.create(name: "Applejack")
+ Ingredient.create(name: "Gin")
+ Ingredient.create(name: "Dark rum")
+ Ingredient.create(name: "Sweet vermouth")
+ Ingredient.create(name: "Peach schnapps")
+ Ingredient.create(name: "Vodka")
+ Ingredient.create(name: "Tequila blanco")
+ Ingredient.create(name: "Spiced rum")
+ Ingredient.create(name: "Mint leaves")
+ Ingredient.create(name: "Scotch")
+ Ingredient.create(name: "Triple sec")
+ Ingredient.create(name: "Southern Comfort")
+ Ingredient.create(name: "Simple syrup")
+ Ingredient.create(name: "Brandy")
+ Ingredient.create(name: "Baileys")
+ Ingredient.create(name: "Frangelico")
+ Ingredient.create(name: "Kahlua")
+ Ingredient.create(name: "Club soda")
+ Ingredient.create(name: "Tonic Water")
+ Ingredient.create(name: "Coca-Cola")
+ Ingredient.create(name: "7Up")
+ Ingredient.create(name: "Orange juice")
+ Ingredient.create(name: "Angostura Bitters")
+ Ingredient.create(name: "Orange bitters")
+ Ingredient.create(name: "Lemon juice")
